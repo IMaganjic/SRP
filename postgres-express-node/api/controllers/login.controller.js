6160ba9487663c594f6362af09dd1f3fd6b9db6c
fesbruby <56271853+fesbruby@users.noreply.github.com>
2020-11-06 00:49:23 +0100

    Test express web server

---
+++ postgres-express-node/api/controllers/login.controller.js
@@ -0,0 +1,18 @@
+const winston = require("winston");
+//const { userServiceInstance } = require("../../services");
+
+const Logger = winston.loggers.get("logger");
+
+
+
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    //const user = await userServiceInstance.createUser({ username, password });
+    res.json({ message: "Login successful" });
+  } catch (err) {
+    Logger.error(err);
+    return res.status(400).json({ error: { message: err.message } });
+  }
+};
